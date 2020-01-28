333583ea173b7f684c12d056adafbeb2bf54eaa8
arcadius4991 <arkadius4991@gmail.com>
2020-01-28 04:24:26 -0600

    added map bounds, fitBounds, and panToBounds functionality

--- Warehousely/Warehousely/wwwroot/scripts/render_map.js
+++ Warehousely/Warehousely/wwwroot/scripts/render_map.js
@@ -1,8 +1,8 @@
 ﻿function initMap() {
     var usaCoords = { lat: 37.0902, lng: -95.7129 };
-
+    var bounds = new google.maps.LatLngBounds();
     var googleMap = new google.maps.Map(
-        document.getElementById('map'), { zoom: 6, center: usaCoords });
+        document.getElementById('map'), { zoom: 6, center: usaCoords, styles: hideStyle });
 
     var mapItems = modelData;
 
@@ -12,6 +12,36 @@
             position: markerPosition,
             map: googleMap
         });
+
+        var location = new google.maps.LatLng(markerPosition.lat, markerPosition.lng);
+        bounds.extend(location);
     };
+
+    googleMap.fitBounds(bounds);
+    googleMap.panToBounds(bounds);
+
+    var hideStyle =
+        [
+            {
+                "featureType": "poi",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            }
+        ];
+
+
 
 }
