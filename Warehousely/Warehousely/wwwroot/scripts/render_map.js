a93800074829456ce032647fc21b14e4a345a52e
arcadius4991 <arkadius4991@gmail.com>
2020-02-02 13:05:19 -0600

    Removed map POIs, maptype controls, streetview icons

--- Warehousely/Warehousely/wwwroot/scripts/render_map.js
+++ Warehousely/Warehousely/wwwroot/scripts/render_map.js
@@ -1,30 +1,60 @@
 ﻿function initMap() {
+    var hidePoi = [
+        {
+            "featureType": "poi",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ];
+
     var usaCoords = { lat: 37.0902, lng: -95.7129 };
     var bounds = new google.maps.LatLngBounds();
     var googleMap = new google.maps.Map(
-        document.getElementById('map'), { zoom: 6, center: usaCoords });
+        document.getElementById('map'), {
+            zoom: 6,
+            center: usaCoords,
+            styles: hidePoi,
+            streetViewControl: false,
+            mapTypeControl: false
+    });
 
     var mapItems = modelData;
+    var infowindow = new google.maps.InfoWindow();
 
     for (var i in mapItems) {
         console.log(mapItems[i].MapsContent);
         var markerPosition = { lat: mapItems[i].lat, lng: mapItems[i].lng };
-        var infowindow = new google.maps.InfoWindow({
-            content: mapItems[i].mapsContent
-        });
 
         var marker = new google.maps.Marker({
             position: markerPosition,
             map: googleMap,
-            infowindow: infowindow
+            infoContent: mapItems[i].mapsContent
 
         });
 
         google.maps.event.addListener(marker, 'click', (function (marker, infowindow) {
             return function () {
-                infowindow.open(googleMap, marker);
+                infowindow.setContent(this.infoContent);
+                infowindow.open(googleMap, this);
             };
         })(marker, infowindow));
+
+        google.maps.event.addListener(googleMap, 'click', (function (infowindow) {
+            return function () {
+                infowindow.close();
+            };
+        })(infowindow));
 
         bounds.extend(markerPosition);
         googleMap.fitBounds(bounds);
